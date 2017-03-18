6f5cb99bec8c89edf17d33c991a44c7b4e56beb7
renanbastos93 <renanbastos.tec@gmail.com>
2017-03-18 18:56:51 -0300

    [+] init test

---
+++ tests/tests.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const API = require('../escapejs');
+
+var arr = ['<script>', ['asdasd', '<script>', ['<asdasd>', ['a', '<script>', {'a': '12', 'b': '<script>'}]]]]
+var obj = {
+	'a': '<script>',
+	'b': ['a', '<script>', {'a': 1, 'b': '<script>'}]
+};
+
+console.log('### API STR ###');
+console.log(API.str('<script>'));
+console.log('-------------------');
+console.log('### API ARRAY ###');
+console.log(JSON.stringify(API.array(arr)));
+console.log('-------------------');
+console.log('### API JSON ###');
+console.log(API.json(obj));
