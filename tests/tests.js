5076b57b74ce640f8648cb70581d4ee506a9750d
renanbastos93 <renanbastos.tec@gmail.com>
2017-03-24 19:07:10 -0300

    [ * ] nomenclature

--- tests/tests.js
+++ tests/tests.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var assert = require('assert');
-const EscapeJS = require('../escapejs');
+const escapeJS = require('../escapejs');
 
 
 describe('Testing of the recursive escape', function(){
@@ -9,7 +9,7 @@ describe('Testing of the recursive escape', function(){
 	it('send string must return string', function(){
 		var str = "<string>";
 		var resStr = "%3Cstring%3E";
-		assert.equal(resStr, EscapeJS.str(str), 'Testing String');
+		assert.equal(resStr, escapeJS.str(str), 'Testing String');
 	});
 
 	it('send json must return json', function(){
@@ -18,7 +18,7 @@ describe('Testing of the recursive escape', function(){
 			'b': ['a', '<script>', {'a': 1, 'b': '<script>'}]
 		};
 		var json = { a: '%3Cscript%3E', b: [ 'a', '%3Cscript%3E', { a: 1, b: '%3Cscript%3E' } ] };
-		assert.deepEqual(json, EscapeJS.json(obj), 'Testing JSON');
+		assert.deepEqual(json, escapeJS.json(obj), 'Testing JSON');
 	});
     
 	it('send array must return array', function(){
@@ -31,14 +31,14 @@ describe('Testing of the recursive escape', function(){
 			]
 		];
 		var array = [ '%3Cscript%3E', [ 'asdasd', '%3Cscript%3E', [ '%3Casdasd%3E', [Object] ] ] ];
-		assert.notStrictEqual(array, EscapeJS.array(arr), 'Testing Array');
+		assert.notStrictEqual(array, escapeJS.array(arr), 'Testing Array');
 	});
 
 	it('send any parameter and return a valid parameter', function(){
 		//String
 		var str = "<string>";
 		var resStr = "%3Cstring%3E";
-		assert.equal(resStr, EscapeJS.str(str), 'Testing String');
+		assert.equal(resStr, escapeJS.str(str), 'Testing String');
 
 		//JSON
 		var obj = {
@@ -46,7 +46,7 @@ describe('Testing of the recursive escape', function(){
 			'b': ['a', '<script>', {'a': 1, 'b': '<script>'}]
 		};
 		var json = { a: '%3Cscript%3E', b: [ 'a', '%3Cscript%3E', { a: 1, b: '%3Cscript%3E' } ] };
-		assert.deepEqual(json, EscapeJS.json(obj), 'Testing JSON');
+		assert.deepEqual(json, escapeJS.json(obj), 'Testing JSON');
 		
 		//Array
 		var arr = [
@@ -58,6 +58,6 @@ describe('Testing of the recursive escape', function(){
 			]
 		];
 		var array = [ '%3Cscript%3E', [ 'asdasd', '%3Cscript%3E', [ '%3Casdasd%3E', [Object] ] ] ];
-		assert.notStrictEqual(array, EscapeJS.array(arr), 'Testing Array');
+		assert.notStrictEqual(array, escapeJS.array(arr), 'Testing Array');
 	});
 });
