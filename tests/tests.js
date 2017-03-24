e0dee0270b3eb2e51623a37a19974cdf0b2af362
renanbastos93 <renanbastos.tec@gmail.com>
2017-03-24 02:16:23 -0300

    [*] improved tests and running good

--- tests/tests.js
+++ tests/tests.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var assert = require('assert');
-const API = require('../escapejs');
+const EscapeJS = require('../escapejs');
 
 
 describe('Testing of the recursive escape', function(){
@@ -9,7 +9,7 @@ describe('Testing of the recursive escape', function(){
 	it('send string must return string', function(){
 		var str = "<string>";
 		var resStr = "%3Cstring%3E";
-		assert.equal(resStr, API.str(str), 'Testing String');
+		assert.equal(resStr, EscapeJS.str(str), 'Testing String');
 	});
 
 	it('send json must return json', function(){
@@ -17,8 +17,8 @@ describe('Testing of the recursive escape', function(){
 			'a': '<script>',
 			'b': ['a', '<script>', {'a': 1, 'b': '<script>'}]
 		};
-		var json = API.json(obj);
-		assert.equal(json, API.json(obj), 'Testing JSON');
+		var json = { a: '%3Cscript%3E', b: [ 'a', '%3Cscript%3E', { a: 1, b: '%3Cscript%3E' } ] };
+		assert.deepEqual(json, EscapeJS.json(obj), 'Testing JSON');
 	});
     
 	it('send array must return array', function(){
@@ -30,7 +30,34 @@ describe('Testing of the recursive escape', function(){
 				]
 			]
 		];
-		var array = API.array(arr);
-		assert.equal(array, API.array(arr), 'Testing Array');
+		var array = [ '%3Cscript%3E', [ 'asdasd', '%3Cscript%3E', [ '%3Casdasd%3E', [Object] ] ] ];
+		assert.notStrictEqual(array, EscapeJS.array(arr), 'Testing Array');
+	});
+
+	it('send any parameter and return a valid parameter', function(){
+		//String
+		var str = "<string>";
+		var resStr = "%3Cstring%3E";
+		assert.equal(resStr, EscapeJS.str(str), 'Testing String');
+
+		//JSON
+		var obj = {
+			'a': '<script>',
+			'b': ['a', '<script>', {'a': 1, 'b': '<script>'}]
+		};
+		var json = { a: '%3Cscript%3E', b: [ 'a', '%3Cscript%3E', { a: 1, b: '%3Cscript%3E' } ] };
+		assert.deepEqual(json, EscapeJS.json(obj), 'Testing JSON');
+		
+		//Array
+		var arr = [
+			'<script>', 
+			['asdasd', '<script>', 
+				['<asdasd>', 
+					['a', '<script>', {'a': '12', 'b': '<script>'}]
+				]
+			]
+		];
+		var array = [ '%3Cscript%3E', [ 'asdasd', '%3Cscript%3E', [ '%3Casdasd%3E', [Object] ] ] ];
+		assert.notStrictEqual(array, EscapeJS.array(arr), 'Testing Array');
 	});
 });
