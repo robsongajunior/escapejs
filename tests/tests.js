d5409d93fbce5db58b38dab1a00b5ef26830be0e
renanbastos93 <renanbastos.tec@gmail.com>
2017-04-17 23:12:09 -0300

    [*] running test before comparable version

--- tests/tests.js
+++ tests/tests.js
@@ -1,36 +1,8 @@
-'use strict';
+var versions = process.versions.node;
+var numVersion = parseInt(versions.replace('.', ''));
 
-var assert = require('assert');
-const {escap} = require('../escapejs');
-
-
-describe('Testing of the recursive escape', function(){
-
-	it('send string must return string', function(){
-		var str = "<string>";
-		var resStr = "%3Cstring%3E";
-		assert.equal(resStr, escap(str), 'Testing String');
-	});
-
-	it('send json must return json', function(){
-		var obj = {
-			'a': '<script>',
-			'b': ['a', '<script>', {'a': 1, 'b': '<script>'}]
-		};
-		var json = { a: '%3Cscript%3E', b: [ 'a', '%3Cscript%3E', { a: 1, b: '%3Cscript%3E' } ] };
-		assert.deepEqual(json, escap(obj), 'Testing JSON');
-	});
-    
-	it('send array must return array', function(){
-		var arr = [
-			'<script>', 
-			['asdasd', '<script>', 
-				['<asdasd>', 
-					['a', '<script>', {'a': '12', 'b': '<script>'}]
-				]
-			]
-		];
-		var array = [ '%3Cscript%3E', [ 'asdasd', '%3Cscript%3E', [ '%3Casdasd%3E', [Object] ] ] ];
-		assert.notStrictEqual(array, escap(arr), 'Testing Array');
-	});
-});
+if(versions === '6.4.0' || numVersion >= 64){
+	require('./tests_es6');
+}else{
+	require('./tests_es5');
+}
